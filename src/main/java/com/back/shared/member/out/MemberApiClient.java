f206d8a77b6dbe2f293b28f3386743218c6f5197
123 <a123@Mac-Pro.local>
2025-12-24 12:16:32 +0900

    0016

--- src/main/java/com/back/shared/member/out/MemberApiClient.java
+++ src/main/java/com/back/shared/member/out/MemberApiClient.java
@@ -6,11 +6,11 @@ import org.springframework.web.client.RestClient;
 @Service
 public class MemberApiClient {
 
-    private static final RestClient restClient = RestClient.builder()
+    private final RestClient restClient = RestClient.builder()
             .baseUrl("http://localhost:8080/member/api/v1")
             .build();
 
-    public static String getRandomSecureTip() {
+    public String getRandomSecureTip() {
         return restClient.get()
                 .uri("/members/randomSecureTip")
                 .retrieve()
