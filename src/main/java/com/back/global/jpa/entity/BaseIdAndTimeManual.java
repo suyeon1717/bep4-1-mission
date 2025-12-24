45f6bd573172a2c83dd24b438cf25c8d494d91c7
123 <a123@Mac-Pro.local>
2025-12-24 15:10:55 +0900

    0016

--- src/main/java/com/back/global/jpa/entity/BaseIdAndTimeManual.java
+++ src/main/java/com/back/global/jpa/entity/BaseIdAndTimeManual.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @Getter
 public abstract class BaseIdAndTimeManual extends BaseEntity {
+    // id, createDate, modifyDate 칼럼에 자동 값 등록 옵션 제거
 
     @Id
     private int id;
