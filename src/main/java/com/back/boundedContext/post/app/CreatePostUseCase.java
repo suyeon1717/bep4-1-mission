04cef4421ebd87a8a36690f875a98fd79045fae6
123 <a123@Mac-Pro.local>
2025-12-24 09:51:43 +0900

    0013

--- src/main/java/com/back/boundedContext/post/app/CreatePostUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/CreatePostUseCase.java
@@ -20,9 +20,7 @@ public class CreatePostUseCase {
 
     public RsData<Post> createPost(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
-
         eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
-//        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
 
         String randomSecureTip = memberFacade.getRandomSecureTip();
 
