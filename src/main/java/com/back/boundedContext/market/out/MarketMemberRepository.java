85583c3aaae0bad25952abed08dbf34efbe24910
123 <a123@Mac-Pro.local>
2025-12-29 10:56:39 +0900

    0032

--- src/main/java/com/back/boundedContext/market/out/MarketMemberRepository.java
+++ src/main/java/com/back/boundedContext/market/out/MarketMemberRepository.java
@@ -3,5 +3,8 @@ package com.back.boundedContext.market.out;
 import com.back.boundedContext.market.domain.MarketMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MarketMemberRepository extends JpaRepository<MarketMember, Integer> {
+    Optional<MarketMember> findByUsername(String username);
 }
