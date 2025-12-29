90474e59717fbb473e6d3c3cdb5168b64994c19d
123 <a123@Mac-Pro.local>
2025-12-29 11:43:00 +0900

    0033

--- src/main/java/com/back/boundedContext/market/app/MarketSyncMemberUseCase.java
+++ src/main/java/com/back/boundedContext/market/app/MarketSyncMemberUseCase.java
@@ -2,6 +2,9 @@ package com.back.boundedContext.market.app;
 
 import com.back.boundedContext.market.domain.MarketMember;
 import com.back.boundedContext.market.out.MarketMemberRepository;
+import com.back.global.eventPublisher.EventPublisher;
+import com.back.shared.market.dto.MarketMemberDto;
+import com.back.shared.market.event.MarketMemberCreatedEvent;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,9 +13,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MarketSyncMemberUseCase {
     private final MarketMemberRepository marketMemberRepository;
+    private final EventPublisher eventPublisher;
 
     public MarketMember syncMember(MemberDto member) {
-        MarketMember marketMember = new MarketMember(
+        boolean isNew = !marketMemberRepository.existsById(member.getId());
+
+        MarketMember _member = marketMemberRepository.save(
+                new MarketMember(
                         member.getId(),
                         member.getCreateDate(),
                         member.getModifyDate(),
@@ -20,8 +27,17 @@ public class MarketSyncMemberUseCase {
                         "",
                         member.getNickname(),
                         member.getActivityScore()
+                )
         );
 
-        return marketMemberRepository.save(marketMember);
+        if(isNew){
+            eventPublisher.publish(
+                    new MarketMemberCreatedEvent(
+                            new MarketMemberDto(_member)
+                    )
+            );
+        }
+
+        return _member;
     }
 }
