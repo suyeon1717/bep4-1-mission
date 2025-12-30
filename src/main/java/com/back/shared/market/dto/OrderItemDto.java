17876edd1e4f1c5068397cc544777aa45732d0a7
123 <a123@Mac-Pro.local>
2025-12-30 14:46:21 +0900

    0044

--- src/main/java/com/back/shared/market/dto/OrderItemDto.java
+++ src/main/java/com/back/shared/market/dto/OrderItemDto.java
@@ -1,5 +1,6 @@
 package com.back.shared.market.dto;
 
+import com.back.standard.modelType.CanGetModelTypeCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +8,7 @@ import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
-public class OrderItemDto {
+public class OrderItemDto implements CanGetModelTypeCode {
     private final int id;
     private final LocalDateTime createDate;
     private final LocalDateTime modifyDate;
@@ -21,4 +22,11 @@ public class OrderItemDto {
     private final long price;
     private final long salePrice;
     private final double payoutRate;
+    private final long payoutFee;
+    private final long salePriceWithoutFee;
+
+    @Override
+    public String getModelTypeCode() {
+        return "OrderItem";
+    }
 }
